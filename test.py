fa327780b77483e91361422b4a2d69824ee16a86
mitul72 <mituldhawan2@gmail.com>
2021-03-28 00:44:09 +0530

    base code

---
+++ test.py
@@ -0,0 +1,21 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication,QMainWindow
+import sys
+
+def window():
+    app=QApplication(sys.argv)
+    win=QMainWindow()
+#   win.setGeometry(xpos,ypos,width,height)
+    win.setGeometry(200,200,700,300)
+    win.setWindowTitle("mitul's application")
+
+    label=QtWidgets.QLabel(win)
+    label.setText("Hello World!")
+    label.move(50,50)
+
+    win.show()
+    sys.exit(app.exec_())
+
+window()
+
+
